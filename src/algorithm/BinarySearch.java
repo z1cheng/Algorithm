77cb398f0e65b9911d73fefffcf325acf6ca9cb1
z1cheng <imchench@gmail.com>
2024-03-05 23:50:10 +0800

    Add binary search algorithm from JDK
    Signed-off-by: z1cheng <imchench@gmail.com>

--- src/algorithm/BinarySearch.java
+++ src/algorithm/BinarySearch.java
@@ -1,9 +1,11 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 
     //<target
-    private static int binarySearch(int[] nums, int target) {
+    public static int binarySearch(int[] nums, int target) {
         int left = 0, right = nums.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
@@ -17,7 +19,7 @@ public class BinarySearch {
     }
 
     // >target
-    private static int binarySearch2(int[] nums, int target) {
+    public static int binarySearch2(int[] nums, int target) {
         //[left, right) 区间
         int left = 0, right = nums.length;
         while (left < right) {
@@ -34,7 +36,7 @@ public class BinarySearch {
     }
 
     //lowerBound, >=target
-    private static int binarySearch3(int[] nums, int target) {
+    public static int binarySearch3(int[] nums, int target) {
         //[left, right) 区间
         int left = 0, right = nums.length;
         while (left < right) {
@@ -54,7 +56,7 @@ public class BinarySearch {
     }
 
     //<=target
-    private static int binarySearch4(int[] nums, int target) {
+    public static int binarySearch4(int[] nums, int target) {
         int left = 0, right = nums.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
@@ -66,5 +68,39 @@ public class BinarySearch {
             }
         }
         return left - 1;
+    }
+
+    /**
+     * 如果找到，直接返回下标（多个相同随机返回）
+     * 否则返回插入点（第一个大于target的下标）
+     * 底层实现，也就是左闭右闭区间查找
+     * private static int binarySearch0(int[] a, int fromIndex, int toIndex,
+     *                                  int key) {
+     *     int low = fromIndex;
+     *     int high = toIndex - 1;
+     *
+     *     while (low <= high) {
+     *         int mid = (low + high) >>> 1;
+     *         int midVal = a[mid];
+     *
+     *         if (midVal < key)
+     *             low = mid + 1;
+     *         else if (midVal > key)
+     *             high = mid - 1;
+     *         else
+     *             return mid; // key found
+     *     }
+     *     return -(low + 1);  // key not found.
+     * }
+     */
+    public static int binarySearchFromJDK(int[] nums, int target) {
+        int idx = Arrays.binarySearch(nums, target);
+        //如果没找到，也就是负数，返回插入点
+        //插入点是第一个大于target的下标
+        if (idx < 0) {
+            //转换
+            return -idx - 1;
+        }
+        return idx;
     }
 }
