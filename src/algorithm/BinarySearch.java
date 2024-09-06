d2c6922c7ec87a7b22644ca114ce79c57cd75b52
z1cheng <imchench@gmail.com>
2024-09-06 23:02:52 +0800

    Add more descriptions for BinarySearch

--- src/algorithm/BinarySearch.java
+++ src/algorithm/BinarySearch.java
@@ -4,12 +4,17 @@ import java.util.Arrays;
 
 public class BinarySearch {
 
+    public static void main(String[] args) {
+        System.out.println(binarySearch2(new int[]{1,2,3,3,5}, 4));
+    }
+
     //<target
+    //找到最后一个小于target的下标
     public static int binarySearch(int[] nums, int target) {
         int left = 0, right = nums.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
+            if (nums[mid] < target) { //最后 left 一定是大于等于 target 的
                 left = mid + 1;
             } else {
                 right = mid;
@@ -18,7 +23,7 @@ public class BinarySearch {
         return left - 1;
     }
 
-    // >target
+    //找到第一个大于target的下标
     public static int binarySearch2(int[] nums, int target) {
         //[left, right) 区间
         int left = 0, right = nums.length;
@@ -26,7 +31,7 @@ public class BinarySearch {
             // 避免溢出
             int mid = left + (right - left) / 2;
             if (nums[mid] <= target) {
-                //在mid>target不会改变left
+                //在mid>target不会改变left，最后 left 是大于 target 的
                 left = mid + 1;
             } else {
                 right = mid;
@@ -36,6 +41,8 @@ public class BinarySearch {
     }
 
     //lowerBound, >=target
+    //找到第一个 target 的下标，比如target 3, nums [1,2,3,3,5] 会返回 2，即[1,2,*3*,3,5]
+    //如果 target 不存在，比如 target 4，则是[1,2,3,*3*,5]
     public static int binarySearch3(int[] nums, int target) {
         //[left, right) 区间
         int left = 0, right = nums.length;
@@ -44,7 +51,7 @@ public class BinarySearch {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
                 //[0, mid] < target
-                //在mid>=target之后不会改变 left
+                //在mid>=target之后不会改变 left，最后 left 是大于等于 target
                 left = mid + 1;
             } else {
                 // nums[mid] >= target
@@ -55,7 +62,8 @@ public class BinarySearch {
         return left;
     }
 
-    //<=target
+    //upperBound，最后一个等于 target 的下标，比如target 3, nums [1,2,3,3,5] 会返回 3，即[1,2,3,*3*,5]
+    //如果 target 不存在，比如 target 4，则是[1,2,3,3,*5*]
     public static int binarySearch4(int[] nums, int target) {
         int left = 0, right = nums.length;
         while (left < right) {
@@ -75,22 +83,20 @@ public class BinarySearch {
      * 否则返回插入点（第一个大于target的下标）
      * 底层实现，也就是左闭右闭区间查找
      * private static int binarySearch0(int[] a, int fromIndex, int toIndex,
-     *                                  int key) {
-     *     int low = fromIndex;
-     *     int high = toIndex - 1;
-     *
-     *     while (low <= high) {
-     *         int mid = (low + high) >>> 1;
-     *         int midVal = a[mid];
-     *
-     *         if (midVal < key)
-     *             low = mid + 1;
-     *         else if (midVal > key)
-     *             high = mid - 1;
-     *         else
-     *             return mid; // key found
-     *     }
-     *     return -(low + 1);  // key not found.
+     * int key) {
+     * int low = fromIndex;
+     * int high = toIndex - 1;
+     * while (low <= high) {
+     * int mid = (low + high) >>> 1;
+     * int midVal = a[mid];
+     * if (midVal < key)
+     * low = mid + 1;
+     * else if (midVal > key)
+     * high = mid - 1;
+     * else
+     * return mid; // key found
+     * }
+     * return -(low + 1);  // key not found.
      * }
      */
     public static int binarySearchFromJDK(int[] nums, int target) {
